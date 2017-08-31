4874efbee3871f87f2fcb37e8bee4beb750e9924
Christian Jordan Dalan <cjdalan@gmail.com>
2017-08-31 19:45:07 +0800

    Updated longest_substring_in_alphabetical_order

--- longest_substring_in_alphabetical_order.py
+++ longest_substring_in_alphabetical_order.py
@@ -4,7 +4,7 @@ def longest_substring_in_alphabetical_order(string):
 	substring = string[0]
 
 	for index in range(1, len(string)):
-		if is_alphabetical(string[index - 1], string[index]):
+		if ord(string[index - 1]) <= ord(string[index]):
 			substring += string[index]
 		else:
 			if len(substring) > len(longest_substring):
@@ -12,12 +12,6 @@ def longest_substring_in_alphabetical_order(string):
 			substring = string[index]
 
 	return longest_substring
-		
-
-def is_alphabetical(character_one, character_two):
-	if ord(character_one) <= ord(character_two):
-		return True
-	return False
 
 
 if __name__ == '__main__':
